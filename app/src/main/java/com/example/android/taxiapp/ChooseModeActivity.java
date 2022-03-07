79a406641aea56cabe31dfbaef37d868a89c1272
Vyacheslav Sapozhnikov <sapognikovslava@gmail.com>
2022-03-07 17:34:18 +0300

    TaxiApp 1.0

---
+++ app/src/main/java/com/example/android/taxiapp/ChooseModeActivity.java
@@ -0,0 +1,26 @@
+package com.example.android.taxiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ChooseModeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_mode);
+    }
+
+    public void goToPassengerSignIn(View view) {
+        startActivity(new Intent(ChooseModeActivity.this, PassengerSignInActivity.class));
+    }
+
+    public void goToDriverSignIn(View view) {
+        startActivity(new Intent(ChooseModeActivity.this, DriverSignInActivity.class));
+    }
+}
